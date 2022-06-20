fcfac0dc3a7f68c6e975cad254107ce6bed6ae68
Abhiram V <abhi.raa.man.v@gmail.com>
2022-06-20 19:45:43 +0530

    Update server to use raw udp connection
    Remove DTLS from server, since it is not possible to establish DTLS
    connection with each server by the client. We need another way to
    perform client authentication, perhaps using signing.

--- server.c
+++ server.c
@@ -8,8 +8,6 @@
 #include <openssl/err.h>
 #include <getopt.h>
 
-#include "dtls.h"
-
 #define	DEFAULT_PORT	6969
 #define BUFFSIZE	2048
 #define RXBUF_SIZE	BUFFSIZE
@@ -22,6 +20,8 @@ static struct {
 
 static void parse_args(int *argc, char *argv[]);
 
+int create_socket(int domain, int port);
+
 int main(int argc, char *argv[])
 {
 	char rxbuf[RXBUF_SIZE], txbuf[TXBUF_SIZE];
@@ -29,59 +29,59 @@ int main(int argc, char *argv[])
 	int sockfd = -1, rxlen, txlen;
 	struct sockaddr_in servaddr, cliaddr;
 	bool is_server = true;
+	ssize_t ret;
 	socklen_t addrsize = sizeof(servaddr);
-	SSL_CTX *ssl_ctx = NULL;
-	SSL *ssl = NULL;
-	BIO *bio = NULL;
 
 	parse_args(&argc, argv);
 
-	sockfd = create_server_socket(argopts.port);
+	sockfd = create_socket(AF_INET, argopts.port);
 	if (sockfd == -1)
 		exit(EXIT_FAILURE);
 
-	ssl_ctx = create_ssl_context(is_server);
-	configure_server_context(ssl_ctx, "cert.pem", "key.pem");
 	printf("lsdd: listening on port %d\n", argopts.port);
 
 	while (true) {
-		ssl = SSL_new(ssl_ctx);
-		bio = BIO_new_dgram(sockfd, BIO_NOCLOSE);
-		SSL_set_bio(ssl, bio, bio);
-		SSL_set_options(ssl, SSL_OP_COOKIE_EXCHANGE);
-		while (DTLSv1_listen(ssl, (BIO_ADDR *)&cliaddr) <= 0)
-			;
-		/* connection established */
-		printf("%s:%d connected\n",
+		ret = recvfrom(sockfd, rxbuf, sizeof(rxbuf), 0,
+				(struct sockaddr *)&cliaddr, &addrsize);
+		if (ret < 0) {
+			perror("recvfrom error");
+			continue;
+		}
+		printf("received %zd bytes from %s:%d\n", ret,
 			inet_ntop(AF_INET, &cliaddr.sin_addr, addrstr, sizeof(addrstr)),
 			ntohs(cliaddr.sin_port));
-		while (true) {
-			int err = 0;
-			if ((rxlen = readline_ssl(ssl, rxbuf, sizeof(rxbuf)-1, &err)) <= 0) {
-				if (rxlen == 0)
-					puts("client closed connection\n");
-				if (err)
-					ERR_print_errors_fp(stderr);
-				break;
-			}
-			rxbuf[rxlen] = '\0';
-			printf("received:\n%s\n", rxbuf);
-			// process message
-			// send response
-			if (SSL_write(ssl, rxbuf, rxlen) <= 0)
-				ERR_print_errors_fp(stderr);
-		}
 	}
 	printf("server exiting...\n");
-exit:
-	if (ssl != NULL) {
-		SSL_shutdown(ssl);
-		SSL_free(ssl);
-	}
-	SSL_CTX_free(ssl_ctx);
-	if (sockfd != -1)
-		close(sockfd);
+out:
+	close(sockfd);
 	return 0;
+}
+
+int create_socket(int domain, int port)
+{
+	int s, optval = 1;
+	struct sockaddr_in addr;	// supports only ipv4 right now
+
+	s = socket(domain, SOCK_DGRAM, 0);
+	if (s == -1) {
+		perror("error creating socket");
+		return -1;
+	}
+
+	addr.sin_family = domain;
+	addr.sin_port = htons(port);
+	addr.sin_addr.s_addr = INADDR_ANY;
+	if (setsockopt(s, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof(optval)) == -1) {
+		perror("setsockopt(SO_REUSEPORT) failed");
+		fprintf(stderr, "continuing..\n");
+	}
+	if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+		perror("bind error");
+		close(s);
+		return -1;
+	}
+
+	return s;
 }
 
 static void parse_args(int *argc, char *argv[])
