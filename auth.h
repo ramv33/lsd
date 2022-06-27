0159e46eac147eb87fa9c648ad9c9c9a08776e14
Abhiram V <abhi.raa.man.v@gmail.com>
2022-06-27 23:02:16 +0530

    Add functions to sign and verify a message

---
+++ auth.h
@@ -0,0 +1,10 @@
+#ifndef AUTH_H
+#define AUTH_H 1
+
+void signbuf(const char *pvtkey, unsigned char *buf, size_t bufsize, unsigned char **sig,
+		size_t *siglen);
+
+int verifysig(const char *pubkey, unsigned char *buf, size_t bufsize, unsigend char **sig,
+		size_t *siglen);
+
+#endif /* ifndef AUTH_H */
