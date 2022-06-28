52730aefd3420c358802479772737592cf10a693
Abhiram V <abhi.raa.man.v@gmail.com>
2022-06-28 21:09:03 +0530

    Add function send_notification() for force requests
    send_notificaion() simply sends a notification to the user using
    zenity. It does not ask for any confirmation.

--- notif.c
+++ notif.c
@@ -3,6 +3,8 @@
 #include <string.h>
 
 #include "common.h"
+#include "protocol.h"
+#include "notif.h"
 
 int confirm_shutdown(void)
 {
@@ -14,11 +16,37 @@ int confirm_shutdown(void)
 		--title 'Confirm to shutdown' \
 		--text 'Confirm shutdown? (yes/no)'","r");
 	if (fp==NULL) {
-		perror("Pipe returned a error");
+		perror("popen: zenity");
 	} else {
 		int ret = WEXITSTATUS(pclose(fp));
 		PDEBUG("confirmation status code: %d\n", ret);
 		/* zenity returns 0 on 'yes' and 1 on 'no'. negate it */
 		return !ret;
 	}
+}
+
+void send_notification(struct request *req)
+{
+	uint16_t req_type;
+	FILE *fp;
+	char msg[16];
+	char cmd[256];
+
+	req_type = req->req_type;
+	RESET_FORCE_BIT(req_type);
+
+	if (reqstr(req_type, msg, sizeof(msg)) == NULL) {
+		PDEBUG("[*] invalid request type: %x\n", req_type);
+		return;
+	}
+
+	snprintf(cmd, sizeof(cmd), "zenity --info --text='%s in %d seconds'",
+			msg, req->timer);
+
+	fp = popen(cmd, "r");
+	if (fp == NULL)
+		perror("popen: zenity");
+	else
+		PDEBUG("[-] sent notification: '%s in %d seconds'\n",
+			msg, req->timer);
 }
--- notif.h
+++ notif.h
@@ -1,6 +1,9 @@
 #ifndef NOTIF_H
 #define NOTIF_H 1
 
+#include "protocol.h"
+
 int confirm_shutdown(void);
+void send_notification(struct request *req_type);
 
 #endif /* ifndef NOTIF_H */
